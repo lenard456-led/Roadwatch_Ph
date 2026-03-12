f4cd25b648d3243fdef67a796ba569e6e3dcebf5
lenard456-led <130036614+lenard456-led@users.noreply.github.com>
2026-03-12 18:23:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,82 @@
+let reportMap
+let marker
+let lat=0
+let lng=0
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+initReportMap()
+
+})
+
+
+
 function openMenu(){
-let m=document.getElementById("menu");
-m.style.width=(m.style.width=="250px")?"0":"250px";
+
+let m=document.getElementById("menu")
+
+m.style.width =
+m.style.width=="250px"
+? "0"
+: "250px"
+
 }
+
+
 
 function show(id){
-document.querySelectorAll("section")
-.forEach(s=>s.classList.remove("active"));
 
-document.getElementById(id)
-.classList.add("active");
+document
+.querySelectorAll("section")
+.forEach(s=>s.classList.remove("active"))
 
-openMenu();
+document
+.getElementById(id)
+.classList.add("active")
+
+openMenu()
 
 if(id=="submit"){
-setTimeout(initReportMap,300);
+
 setTimeout(()=>{
-reportMap.invalidateSize();
-},500);
+reportMap.invalidateSize()
+},500)
+
 }
+
 }
 
 
-
-let reportMap, marker, lat=0, lng=0;
 
 function initReportMap(){
 
-if(reportMap) return;
+if(reportMap) return
 
-reportMap=L.map('reportMap')
-.setView([14.1,121],10);
+reportMap =
+L.map("reportMap")
+.setView([14.1,121],10)
 
 L.tileLayer(
-'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-).addTo(reportMap);
+"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+).addTo(reportMap)
 
 reportMap.on("click", e=>{
 
-lat=e.latlng.lat;
-lng=e.latlng.lng;
+lat=e.latlng.lat
+lng=e.latlng.lng
 
-if(marker) reportMap.removeLayer(marker);
+if(marker)
+reportMap.removeLayer(marker)
 
-marker=L.marker([lat,lng]).addTo(reportMap);
+marker =
+L.marker([lat,lng])
+.addTo(reportMap)
 
 selectedLocation.innerText =
-lat+", "+lng;
+lat+", "+lng
 
-});
+})
 
 }
 
@@ -55,25 +84,24 @@ lat+", "+lng;
 
 function detectLocation(){
 
-navigator.geolocation.getCurrentPosition(
+navigator.geolocation.getCurrentPosition(pos=>{
 
-pos=>{
+lat=pos.coords.latitude
+lng=pos.coords.longitude
 
-lat=pos.coords.latitude;
-lng=pos.coords.longitude;
+if(marker)
+reportMap.removeLayer(marker)
 
-if(marker) reportMap.removeLayer(marker);
+marker =
+L.marker([lat,lng])
+.addTo(reportMap)
 
-marker=L.marker([lat,lng]).addTo(reportMap);
-
-reportMap.setView([lat,lng],15);
+reportMap.setView([lat,lng],15)
 
 selectedLocation.innerText =
-lat+", "+lng;
+lat+", "+lng
 
-}
-
-);
+})
 
 }
 
@@ -81,7 +109,7 @@ lat+", "+lng;
 
 function generateTracking(){
 
-return "RW-"+Date.now();
+return "RW-"+Date.now()
 
 }
 
@@ -90,14 +118,13 @@ return "RW-"+Date.now();
 async function submitReport(){
 
 if(lat==0){
-alert("Pin location");
-return;
+alert("Pin location")
+return
 }
 
-let tracking=generateTracking();
+let tracking=generateTracking()
 
 let data={
-
 tracking:tracking,
 lastname:lastname.value,
 firstname:firstname.value,
@@ -109,10 +136,7 @@ issue:issue.value,
 latitude:lat,
 longitude:lng,
 status:"Pending"
-
-};
-
-try{
+}
 
 await fetch(
 "https://script.google.com/macros/s/AKfycbywslM_-8Ohowuny4theFIQNNB0TjdhI0MANQSCLQGzy6X1fsmkDXXMz9FBjQfHIXdM/exec",
@@ -123,49 +147,28 @@ headers:{
 },
 body:JSON.stringify(data)
 }
-);
+)
 
-trackInfo.innerHTML=
-"Tracking:<br>"+tracking;
+trackInfo.innerHTML=tracking
 
-popup.style.display="flex";
-
-}
-catch(e){
-
-alert("Failed submit");
-
-}
+popup.style.display="flex"
 
 }
 
 
 
 function closePopup(){
-popup.style.display="none";
-show("home");
+
+popup.style.display="none"
+show("home")
+
 }
+
+
 
 function newReport(){
 
-popup.style.display="none";
-
-document.querySelectorAll(
-"#submit input,#submit textarea"
-).forEach(e=>e.value="");
-
-lat=0;
-lng=0;
-
-if(marker)
-reportMap.removeLayer(marker);
-
-show("submit");
+popup.style.display="none"
+show("submit")
 
 }
-
-
-
-window.onload=()=>{
-initReportMap();
-};
