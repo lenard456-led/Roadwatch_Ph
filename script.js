2423061f728ebe0b69b3bbd577687b96a6687dac
lenard456-led <130036614+lenard456-led@users.noreply.github.com>
2026-03-13 01:31:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -205,32 +205,7 @@ function resetForm() {
 
 
 
-async function loadReports(){
 
-try{
-
-let response = await fetch(API_URL)
-let reports = await response.json()
-
-reports.forEach(r=>{
-
-if(!r.lat || !r.lng) return
-
-L.marker([parseFloat(r.lat), parseFloat(r.lng)])
-.addTo(map)
-.bindPopup(
-"<b>"+r.issue+"</b><br>"+r.location+"<br>Status: "+(r.status || "Pending")
-)
-
-})
-
-}catch(err){
-
-console.log("Error loading reports", err)
-
-}
-
-}
 
 
 
