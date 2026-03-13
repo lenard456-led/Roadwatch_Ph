49d4b0e6f39868d9e1ee0c0316bada4efe90b965
lenard456-led <130036614+lenard456-led@users.noreply.github.com>
2026-03-13 19:57:09 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,16 +1,42 @@
-<div class="loginCard">
+const auth = firebase.auth();
 
-<h2>RoadWatch PH Login</h2>
+function login() {
 
-<input id="email" placeholder="Email">
-<input id="password" type="password" placeholder="Password">
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-<button onclick="login()">Login</button>
+  auth.signInWithEmailAndPassword(email, password)
+  .then(() => {
 
-<button onclick="register()">Register</button>
+    window.location.href = "index.html";
 
-<hr>
+  })
+  .catch(err => alert(err.message));
+}
 
-<button onclick="googleLogin()">Login with Google</button>
+function register() {
 
-</div>
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  auth.createUserWithEmailAndPassword(email, password)
+  .then(() => {
+
+    window.location.href = "index.html";
+
+  })
+  .catch(err => alert(err.message));
+}
+
+function googleLogin() {
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  auth.signInWithPopup(provider)
+  .then(() => {
+
+    window.location.href = "index.html";
+
+  });
+
+}
